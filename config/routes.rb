5dc582bc89a4d171fcef8909e9e3d7530a5f52fd
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-10-23 17:18:52 -0600

    Add categories pages and sign in/up pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+# config/routes.rb
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "groups#index"
+  
+  resources :groups, only: [:index, :new, :create]
 end
