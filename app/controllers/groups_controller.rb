24ddd2eab7a6f40ab57585e356f009a13462b7d9
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-10-23 17:21:18 -0600

    Fix offenses

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,30 +1,29 @@
 # app/controllers/groups_controller.rb
 
 class GroupsController < ApplicationController
-    before_action :authenticate_user!
-  
-    def index
-      @groups = Group.all
-    end
-  
-    def new
-      @group = Group.new
-    end
-  
-    def create
-      @group = Group.new(group_params)
-  
-      if @group.save
-        redirect_to root_path, notice: 'Category created successfully!'
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def group_params
-      params.require(:group).permit(:name, :icon)
+  before_action :authenticate_user!
+
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to root_path, notice: 'Category created successfully!'
+    else
+      render :new
     end
   end
-  
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
+end
