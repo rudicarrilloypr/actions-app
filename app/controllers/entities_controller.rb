9b444e11895b9628164103fb5d0c4c0d252d53e8
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-10-24 12:54:37 -0600

    Add transactions pages

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,3 +1,34 @@
 class EntitiesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_group
+
+  def index
+    @entities = @group.entities.order(created_at: :desc)
+    @total_amount = @entities.sum(:amount)
+  end
+
+  def new
+    @entity = Entity.new
+  end
+
+  def create
+    @entity = @group.entities.build(entity_params)
+    @entity.author = current_user
+
+    if @entity.save
+      redirect_to group_entities_path(@group), notice: 'Transaction added successfully!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def entity_params
+    params.require(:entity).permit(:name, :amount)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_211042) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_184411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "entities", force: :cascade do |t|
     t.string "name"
     t.decimal "amount"
-    t.bigint "user_id", null: false
     t.bigint "group_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_entities_on_author_id"
     t.index ["group_id"], name: "index_entities_on_group_id"
-    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -46,5 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_211042) do
   end
 
   add_foreign_key "entities", "groups"
-  add_foreign_key "entities", "users"
+  add_foreign_key "entities", "users", column: "author_id"
 end
