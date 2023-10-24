6e390a1fbdb29356ab7b3df8802d8ad22f30f02f
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-10-23 19:59:57 -0600

    Fix offenses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to groups_path
-    end
+    return unless user_signed_in?
+
+    redirect_to groups_path
   end
 end
